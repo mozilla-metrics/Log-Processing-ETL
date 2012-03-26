db2e3cc2415920b7cdc806e569db124d194dcbeb
Mark Reid <mreid@mozilla.com>
2012-03-26 16:05:00 -0300

    Added the MPL v2 license header to the utility / example scripts.

--- etl/bin/backprocess_example.sh
+++ etl/bin/backprocess_example.sh
@@ -1,3 +1,7 @@
+# The contents of this file are subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
 # Example command to run a backprocess:
 JAVAMAXMEM=10000 ./kitchen.sh -file /etl/timestamped_log_manager/backprocess.kjb \
     -param:SITE_CONDITION="= 'vamo' AND server = 'nslog1'" \
--- etl/run_cleaner.sh
+++ etl/run_cleaner.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
+# The contents of this file are subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
 find . -type f -not -path "*unused*" -a \( -name "*.ktr" -o -name "*.kjb" -o -name "*.xml" \) -exec ./kettle_xml_cleaner.rb -o -B {} \;
