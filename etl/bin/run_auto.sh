b8d85bb47da9fb7b05042d859ffe30876a57f4d1
Mark Reid <mreid@mozilla.com>
2012-07-02 17:00:36 -0300

    updated with production paths/settings

--- etl/bin/run_auto.sh
+++ etl/bin/run_auto.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-cd /opt/pentaho/kettle/kettle
-#./kitchen.sh -file /opt/pentaho/kettle/etl/logProcessing/run_on_carte.kjb -param:JOB=/etl/timestamped_log_manager/process_missing_logs_wrapper.kjb ${1+"$@"} 2>&1 > auto_cron_hourly.log
-JAVAMAXMEM=20000 ./kitchen.sh -file /opt/pentaho/kettle/etl/logProcessing/etl/timestamped_log_manager/process_missing_logs_wrapper.kjb ${1+"$@"} 2>&1 > auto_cron_hourly.log
+cd /opt/pentaho/kettle/kettle-4.3/
+BASEDIR=/opt/pentaho/kettle/etl/logProcessing
+KETTLE_HOME=$BASEDIR/prod_config
+KETTLE_JOB=$BASEDIR/etl/timestamped_log_manager/process_missing_logs_wrapper.kjb
 
+KETTLE_HOME=$BASEDIR/prod_config JAVAMAXMEM=20000 ./kitchen.sh -file $KETTLE_JOB ${1+"$@"} 2>&1 > auto_cron_hourly.log
--- etl/bin/run_eod.sh
+++ etl/bin/run_eod.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-cd /opt/pentaho/kettle/kettle
-JAVAMAXMEM=10000 ./kitchen.sh -file /opt/pentaho/kettle/etl/logProcessing/etl/timestamped_log_manager/eod_processing.kjb ${1+"$@"} 2>&1 > eod_cron.log
+cd /opt/pentaho/kettle/kettle-4.3/
+BASEDIR=/opt/pentaho/kettle/etl/logProcessing
+KETTLE_HOME=$BASEDIR/prod_config
+KETTLE_JOB=$BASEDIR/etl/timestamped_log_manager/eod_processing.kjb
+
+KETTLE_HOME=$BASEDIR/prod_config JAVAMAXMEM=10000 ./kitchen.sh -file $KETTLE_JOB ${1+"$@"} 2>&1 > eod_cron.log
 
