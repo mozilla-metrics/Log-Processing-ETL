54c37fc048b5012466a822784f78cac9b40b57c3
Mark Reid <mreid@mozilla.com>
2012-07-02 18:17:37 -0300

    moved the log files out of the kettle dir

--- etl/bin/run_auto.sh
+++ etl/bin/run_auto.sh
@@ -5,4 +5,4 @@ BASEDIR=/opt/pentaho/kettle/etl/logProcessing
 KETTLE_HOME=$BASEDIR/prod_config
 KETTLE_JOB=$BASEDIR/etl/timestamped_log_manager/process_missing_logs_wrapper.kjb
 
-KETTLE_HOME=$BASEDIR/prod_config JAVAMAXMEM=20000 ./kitchen.sh -file $KETTLE_JOB ${1+"$@"} 2>&1 > auto_cron_hourly.log
+KETTLE_HOME=$BASEDIR/prod_config JAVAMAXMEM=20000 ./kitchen.sh -file $KETTLE_JOB ${1+"$@"} 2>&1 > $BASEDIR/auto_cron_hourly.log
--- etl/bin/run_eod.sh
+++ etl/bin/run_eod.sh
@@ -5,5 +5,5 @@ BASEDIR=/opt/pentaho/kettle/etl/logProcessing
 KETTLE_HOME=$BASEDIR/prod_config
 KETTLE_JOB=$BASEDIR/etl/timestamped_log_manager/eod_processing.kjb
 
-KETTLE_HOME=$BASEDIR/prod_config JAVAMAXMEM=10000 ./kitchen.sh -file $KETTLE_JOB ${1+"$@"} 2>&1 > eod_cron.log
+KETTLE_HOME=$BASEDIR/prod_config JAVAMAXMEM=10000 ./kitchen.sh -file $KETTLE_JOB ${1+"$@"} 2>&1 > $BASEDIR/eod_cron.log
 
