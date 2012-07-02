6ca26da79395b72fef040a5e068ee1d43f328a25
Mark Reid <mreid@mozilla.com>
2012-07-02 17:07:15 -0300

    pointed the audit script at the production config dir

--- etl/bin/run_audit.sh
+++ etl/bin/run_audit.sh
@@ -2,4 +2,4 @@
 
 cd /opt/pentaho/kettle/kettle-4.3/
 BASEDIR=/opt/pentaho/kettle/etl/logProcessing
-KETTLE_HOME=$BASEDIR/testing JAVAMAXMEM=20000 ./pan.sh -level Minimal -file $BASEDIR/etl/timestamped_log_manager/audit_server_traffic.ktr
+KETTLE_HOME=$BASEDIR/prod_config JAVAMAXMEM=20000 ./pan.sh -level Minimal -file $BASEDIR/etl/timestamped_log_manager/audit_server_traffic.ktr
