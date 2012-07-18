6421bc8d7b29769869a56f36ea6c0eccfd18b818
Mark Reid <mreid@mozilla.com>
2012-07-18 11:40:25 -0300

    added a script to actually update the products

--- etl/bin/run_product_version_management_check.sh
+++ etl/bin/run_product_version_management_check.sh
@@ -4,4 +4,5 @@ BASEDIR=/opt/pentaho/kettle/etl/logProcessing
 cd $BASEDIR/kettle
 LOG=/var/log/etl/dimensions_cron_nightly.log
 
+#KETTLE_HOME=/opt/pentaho/kettle
 ./kitchen.sh -level Minimal -file $BASEDIR/etl/dimensions/product_version_management.kjb -param:mode=check ${1+"$@"} 2>&1 | tee -a $LOG | grep -C 1000 ERROR
