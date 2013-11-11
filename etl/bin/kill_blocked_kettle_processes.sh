75c7a4166c9364389b3d3ef3f80af6cc28458529
Pentaho on ETL1 <pentaho@etl1.metrics.scl3.mozilla.com>
2013-11-11 06:42:43 -0800

    New script to find Kettle processes blocked.

---
+++ etl/bin/kill_blocked_kettle_processes.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# ============================================================================
+# Find Kettle process that don't produce any log output (stalled processes)  #
+# ============================================================================
+
+# Debug settings:
+DEBUG=false
+# How long (minutes) can a process run without log activity:
+STALL_TIME_THRESHOLD=100
+
+KETTLE_PROCESSES=`ps -ef | grep kettle-core.jar | grep -v grep | awk '{print $2}'`
+for KETTLE_PID in $KETTLE_PROCESSES ; do
+	$DEBUG found kettle pid = $KETTLE_PID
+	KETTLE_LOG_FILES=`ls -l /proc/$KETTLE_PID/fd | egrep '.log$' | cut -d '>'  -f 2`
+	$DEBUG Logfiles for $KETTLE_PID: $KETTLE_LOG_FILES
+	# log files changed less than $STALL_TIME_THRESHOLD minutes ago:
+	OLD_LOG_FILES=`find $KETTLE_LOG_FILES -cmin -$STALL_TIME_THRESHOLD`
+	if [ -z "$OLD_LOG_FILES" ]; then
+		echo "========================================================================================================"
+		echo "Found a Kettle process (PID=$KETTLE_PID) without logs activity for at lest $STALL_TIME_THRESHOLD minutes."
+		KETTLE_PPID=`ps -f -p $KETTLE_PID | awk '!/PPID/ {print $3}'`
+		echo "Parent (PID=$KETTLE_PPID) process tree:"
+		pstree -A -a -l $KETTLE_PPID | uniq 
+		echo -n Killing process $KETTLE_PID
+		echo kill
+		echo "  Done."
+		echo "========================================================================================================"
+		echo
+	fi
+done
+
+
+
+
