c672cf0e1a92bfb825f7b277971fbd2a46466063
Ricardo Fradinho <ricardo.fradinho@webdetails.pt>
2013-11-24 20:04:02 +0000

    Minor fix to kill_blocked_kettle_processes. Make sure process site logs exits ok when no files where found/processed.

--- etl/bin/kill_blocked_kettle_processes.sh
+++ etl/bin/kill_blocked_kettle_processes.sh
@@ -10,21 +10,22 @@ DEBUG=false
 STALL_TIME_THRESHOLD=100
 
 KETTLE_PROCESSES=`ps -ef | grep kettle-core.jar | grep -v grep | awk '{print $2}'`
-for KETTLE_PID in $KETTLE_PROCESSES ; do
+for KETTLE_PID in $KETTLE_PROCESSES ;
+do
 	$DEBUG found kettle pid = $KETTLE_PID
-	KETTLE_LOG_FILES=`ls -l /proc/$KETTLE_PID/fd | egrep '.log$' | cut -d '>'  -f 2`
+	KETTLE_LOG_FILES=`ls -l /proc/$KETTLE_PID/fd | egrep '.log$' | cut -d '>' -f 2 2>/dev/null`
 	$DEBUG Logfiles for $KETTLE_PID: $KETTLE_LOG_FILES
 	# log files changed less than $STALL_TIME_THRESHOLD minutes ago:
 	OLD_LOG_FILES=`find $KETTLE_LOG_FILES -cmin -$STALL_TIME_THRESHOLD`
 	if [ -z "$OLD_LOG_FILES" ]; then
 		echo "========================================================================================================"
-		echo "Found a Kettle process (PID=$KETTLE_PID) without logs activity for at lest $STALL_TIME_THRESHOLD minutes."
+		echo "Found a Kettle process (PID=$KETTLE_PID) without logs activity for at least $STALL_TIME_THRESHOLD minutes."
 		KETTLE_PPID=`ps -f -p $KETTLE_PID | awk '!/PPID/ {print $3}'`
 		echo "Parent (PID=$KETTLE_PPID) process tree:"
-		pstree -A -a -l $KETTLE_PPID | uniq 
-		echo -n Killing -9 process $KETTLE_PID
-		kill -9 $KETTLE_PID
-		echo "  Done."
+		pstree -A -a -l "$KETTLE_PPID" | uniq 
+		echo -n "Killing -9 process $KETTLE_PID ..."
+		kill -9 "$KETTLE_PID"
+		echo "  done."
 		echo "========================================================================================================"
 		echo
 	fi
