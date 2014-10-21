044352c95c39984d186d9d91943b35699c5eab18
Pentaho on ETL1 <pentaho@etl1.metrics.scl3.mozilla.com>
2014-10-21 11:16:13 -0700

    Have "run_auto.sh" use kettle wrapper.

--- etl/bin/run_auto.sh
+++ etl/bin/run_auto.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
+export JAVAMAXMEM='20g -Xms6000'
+cd /opt/pentaho/kettle/etl/logProcessing/kettle
 
-BASEDIR=/opt/pentaho/kettle/etl/logProcessing
 
-cd $BASEDIR/kettle
+/opt/pentaho/kettle/etl/weblogs/kettle_wrapper.sh \
+ -j "logProcessingEtl" \
+ -l "Log Processing ETL" \
+ -w 1500 \
+ -c "./kitchen.sh -norep -file /opt/pentaho/kettle/etl/logProcessing/etl/timestamped_log_manager/process_missing_logs.kjb -level Basic ${1+"$@"}"
 
-KETTLE_JOB=$BASEDIR/etl/timestamped_log_manager/process_missing_logs_wrapper.kjb
-LOG=/var/log/etl/auto_cron_hourly_`date '+%Y%m%d_%H%M%S'`.log
-
-#KETTLE_HOME=/opt/pentaho/kettle
-JAVAMAXMEM=20000 ./kitchen.sh -file $KETTLE_JOB -level Detailed ${1+"$@"} > $LOG 2>&1 
-
-KETTLE_EXIT=$?
-
-if [ "$KETTLE_EXIT" -ne "0" ]; then
-   echo "Error encountered during hourly processing. Kettle exit code was '$KETTLE_EXIT', but we expected '0'."
-   cat $LOG
-fi
